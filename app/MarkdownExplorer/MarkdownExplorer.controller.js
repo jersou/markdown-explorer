866f521517ca1eca476b82eb7ffd6a7ae50e0d4b
Jerem <jer.sou@gmail.com>
2018-09-10 00:02:42 +0200

    feat(gui): focus on filter at init

--- app/MarkdownExplorer/MarkdownExplorer.controller.js
+++ app/MarkdownExplorer/MarkdownExplorer.controller.js
@@ -1,9 +1,22 @@
 class MarkdownExplorerController {
-	constructor(MarkdownExplorerService, ConfigService) {
+	constructor(MarkdownExplorerService, ConfigService, $timeout) {
 		Object.assign(this, {
 			MarkdownExplorerService,
 			ConfigService,
+			$timeout,
 		});
+	}
+
+	$onInit() {
+		// FIXME
+		this.$timeout(() => {
+			const element = angular.element(
+				document.getElementsByClassName('filter-input')
+			);
+			if (element && element[0]) {
+				element[0].focus();
+			}
+		}, 1000);
 	}
 
 	toggleIncludeFoldrNameFilter() {
